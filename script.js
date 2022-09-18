a8cc3b62483ad656eec77ede71c7c5df4dd8f241
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2022-09-17 22:58:15 -0400

    Finished exercise for Simple Todo List app.
    This is from ZTM Complete Webdev, Lecture 142. I must say I'm proud of
    what I did; I wasn't expecting the exercise to be this interesting!
    Then again, practice makes perfect, and experience begins to compound
    on itself. But I digress. :)
    
    I took a few liberties with the HTML layout, mainly encapsulating the
    li element with its delete-button in a separate container div with
    class "entry". This way, I could style the delete-button's position
    more appropriately, using in fact a technique we learned earlier in
    the course (using an auto-margin with a flexbox item).

---
+++ script.js
@@ -0,0 +1,55 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+
+// Add strikethrough facility to all existing li elements
+const todoItems = Array.from(document.querySelectorAll("li"));
+todoItems.forEach(todo => todo.addEventListener("click", () => { todo.classList.toggle("done"); }));
+
+// Add delete facility to all existing "Delete" buttons
+const deleteButtons = Array.from(document.querySelectorAll(".delete"));
+deleteButtons.forEach(dbutton => dbutton.addEventListener("click", e => e.target.parentNode.remove()));
+
+function createListElement() {
+    var li = document.createElement("li");
+    li.addEventListener("click", () => { li.classList.toggle("done"); });
+    li.appendChild(document.createTextNode(input.value));
+
+    const div = document.createElement("div");
+    div.classList.add("entry");
+    div.appendChild(li);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete"); // for consistency with existing delete-buttons
+    deleteButton.appendChild(document.createTextNode("Delete"));
+    deleteButton.addEventListener("click", e =>	e.target.parentNode.remove()); // remove parent div
+    
+    div.appendChild(deleteButton);
+    
+    ul.appendChild(div);
+    
+    input.value = "";
+}
+
+function inputLength () {
+    return input.value.length;
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+	    createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+	    createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+
